18b4bbf3e790bdbf2af6694aaf518c5548a8c26a
mysteres <me@suero.io>
2023-07-19 16:05:07 -0400

    better reader test module, needs more

--- c_lib/reader_test.c
+++ c_lib/reader_test.c
@@ -5,22 +5,43 @@
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-char * string = "TESTSTRINGWITHSPACE BLAHBLAHBLAH";
+#ifndef TEST_DEBUG
+#define TEST_DEBUG 1
+#endif
 
-ssize_t read (int fd, void *buf, size_t nbytes) {
-    ssize_t ret = MIN(nbytes, strlen(string));
-    memcpy(buf, string, ret);
-    printf("READ RETURNED: ");
-    for (int i = 0; i < ret; i++) {
-        printf("%c", ((char *)buf)[i]);
+char * buffer = "";
+
+ssize_t read(int fd, void *buf, size_t nbytes) {
+    ssize_t ret = MIN(nbytes, strlen(buffer));
+    memcpy(buf, buffer, ret);
+    if (TEST_DEBUG) {
+        printf("READ RETURNED: ");
+        for (int i = 0; i < ret; i++) {
+            printf("%c", ((char *)buf)[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
-    string = string + ret; // duh
+    buffer = buffer + ret; // duh
     return ret; 
+}
+
+void testReadUntil(reader * reader) {
+    printf("Test Func: testReadUntil\n");
+    buffer = "TESTSTRINGWITHSPACE BLAHBLAHBLAH";
+    char * ret = (char *)reader_read_until(reader, ' ');
+    if (TEST_DEBUG) {
+        printf("Test String: %s\n", buffer);
+        printf("%s = readUntil(' ')\n", ret);
+    }
+    if (strcmp(ret, "TESTSTRINGWITHSPACE") == 0) {
+        printf("Test Passed!\n");
+    } else {
+        printf("Test Failed!\n");
+    }
 }
 
 int main() {
     reader * reader = reader_constructor(-1);
-    printf("%s\n", reader_read_until(reader, ' '));
+    testReadUntil(reader);
     return 0;
 }

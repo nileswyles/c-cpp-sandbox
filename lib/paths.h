7cfc0a0d401012baa4355fc7b644f300e8a32e22
Senor Corazon <merphintime7@gmail.com>
2024-04-21 20:17:35 -0400

    made more portable... becausse no need to obfuscate anyway

--- lib/paths.h
+++ lib/paths.h
@@ -2,11 +2,24 @@
 using namespace std;
 
 // Assuming unix paths for now... 
-// TODO: extend using classes? to support Windows?
-namespace SueroLibs::Paths {
+namespace WylesLibs::Paths {
 
-extern string getParent(string path);
-extern string getFilename(string path);
-extern string getExtension(string path);
+extern string getParent(string path) {
+    size_t pos = path.find_last_of("/");
+    return pos == string::npos ? path : path.substr(0, pos);
+}
+
+extern string getFilename(string path) {
+    size_t pos = path.find_last_of("/");
+    string name_w_ext = pos == string::npos ? path : path.substr(pos);
+
+    size_t endpos = path.find_last_of(".");
+    return endpos == string::npos ? name_w_ext : path.substr(pos, endpos-pos);
+}
+
+extern string getExtension(string path) {
+    size_t pos = path.find_last_of(".");
+    return pos == string::npos ? path : path.substr(pos);
+}
 
 }

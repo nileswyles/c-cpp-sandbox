00958d11acfc420042a4d375902a1c606dbe8812
Senor Corazon <merphintime7@gmail.com>
2024-04-21 20:29:45 -0400

    how can I forget the ifndef guards

--- lib/paths.h
+++ lib/paths.h
@@ -1,15 +1,18 @@
+#ifndef WYLESLIBS_PATHS_H
+#define WYLESLIBS_PATHS_H
+
 #include <string>
 using namespace std;
 
 // Assuming unix paths for now... 
 namespace WylesLibs::Paths {
 
-extern string getParent(string path) {
+static string getParent(string path) {
     size_t pos = path.find_last_of("/");
     return pos == string::npos ? path : path.substr(0, pos);
 }
 
-extern string getFilename(string path) {
+static string getFilename(string path) {
     size_t pos = path.find_last_of("/");
     string name_w_ext = pos == string::npos ? path : path.substr(pos);
 
@@ -17,9 +20,11 @@ extern string getFilename(string path) {
     return endpos == string::npos ? name_w_ext : path.substr(pos, endpos-pos);
 }
 
-extern string getExtension(string path) {
+static string getExtension(string path) {
     size_t pos = path.find_last_of(".");
     return pos == string::npos ? path : path.substr(pos);
 }
 
 }
+
+#endif WYLESLIBS_PATHS_H

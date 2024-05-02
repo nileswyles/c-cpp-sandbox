b535615c0715fa724128f8798d55eb17de3c0079
Senor Corazon <merphintime7@gmail.com>
2024-05-02 18:28:48 -0400

    cdd

--- lib/file.h
+++ lib/file.h
@@ -11,11 +11,11 @@
 namespace WylesLibs::File {
 
 static reader * read(const char * file_path) {
-    int fd = open(file_path, O_READONLY);
-    reader * reader = reader_constructor(fd, READER_RECOMMENDED_BUF_SIZE);
+    int fd = open(file_path, O_RDONLY);
+    reader * r = reader_constructor(fd, READER_RECOMMENDED_BUF_SIZE);
     // so I am not a complete retard...
-    reader * copy = reader_read_until(reader, (char)EOF);
-    reader_destructor(reader);
+    reader * copy = reader_read_until(r, (char)EOF);
+    reader_destructor(r);
     return copy;
 }
 

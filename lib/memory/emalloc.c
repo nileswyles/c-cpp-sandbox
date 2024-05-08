8c3b6ff4500ed9dc2f5cbd0eb5b24ab673ac99e8
Kanye West <KanyeEast@notreally.com>
2024-05-08 06:51:03 -0400

    switched to indicies instead of pointer maths

--- lib/memory/emalloc.c
+++ lib/memory/emalloc.c
@@ -20,7 +20,7 @@ static MemoryHeapNode * usedRootNode = NULL;
 
 static inline void initializeData() {
     if (freedRootNode == NULL) {
-        nodes[0].ptr = (void *)dynamic_memories;
+        nodes[0].index = 0;
         nodes[0].block_size = DYNAMIC_MEMORY_SIZE;
         nodes[0].child = NULL;
         memoryHeapPush(&freedRootNode, nodes);
@@ -39,29 +39,26 @@ extern void * emalloc(size_t size) {
     } 
 
     MemoryHeapNode * node = memoryHeapPop(&freedRootNode, sizeHeapPopCondition, &size);
-    if (node == NULL || node->block_size < size) { 
+    uint32_t nodes_index = node->index;
+    if (node == NULL || node->block_size < size || 0 > nodes_index || nodes_index >= DYNAMIC_MEMORY_SIZE) { 
+
         // freak out 
         return NULL;
     } 
-
-    void * extracted_ptr = node->ptr;
+    void * extracted_ptr = (void *)(dynamic_memories + nodes_index);
     if (node->block_size == size) {
         memoryHeapPush(&usedRootNode, node);
     } else { 
-        void * start_of_memory = (void *)dynamic_memories;
-        printf("%lx, %lx\n", extracted_ptr, &(*dynamic_memories));
-        size_t nodes_index = 0;
-        // size_t nodes_index = extracted_ptr - start_of_memory;
 
         // see check above...
         // if (node->block_size > size) {
-        node->ptr += size;
+        node->index += size;
         node->block_size -= size;
  
         // re-insert updated node (in freed list), with new size...
         memoryHeapPush(&freedRootNode, node);
 
-        nodes[nodes_index].ptr = extracted_ptr;
+        nodes[nodes_index].index = nodes_index;
         nodes[nodes_index].block_size = size;
         nodes[nodes_index].child = NULL;
   
@@ -71,17 +68,16 @@ extern void * emalloc(size_t size) {
 }
 
 extern void efree(void * ptr) {
-    MemoryHeapNode * freed = memoryHeapPop(&usedRootNode, ptrHeapPopCondition, ptr);
-
-    // search for found_contigious where found_contigious->ptr < ptr and found_contigious->block_size + found_cointigious->ptr == ptr.
-    MemoryHeapNode * found_contigious = memoryHeapPop(&freedRootNode, mergeHeapPopCondition, ptr);
-
-    if (found_contigious == NULL) {
-        memoryHeapPush(&freedRootNode, freed);
-    } else {
-        found_contigious->block_size += freed->block_size;
-        memoryHeapPush(&freedRootNode, found_contigious);
+    uint32_t index = (uint8_t *)ptr - (uint8_t *)dynamic_memories;
+    if (0 <= index && index < DYNAMIC_MEMORY_SIZE) {
+        MemoryHeapNode * freed = memoryHeapPop(&usedRootNode, ptrHeapPopCondition, &index);
+        MemoryHeapNode * found_contigious = memoryHeapPop(&freedRootNode, mergeHeapPopCondition, &index);
+        if (found_contigious == NULL) {
+            memoryHeapPush(&freedRootNode, freed);
+        } else {
+            found_contigious->block_size += freed->block_size;
+            memoryHeapPush(&freedRootNode, found_contigious);
+        }
+        // yeah, this is actually pretty clean! I like the solution, assuming that mergeHeadPopCondition stuff works.
     }
-
-    // yeah, this is actually pretty clean! I like the solution, assuming that mergeHeadPopCondition stuff works.
 }
--- lib/memory/heap.h
+++ lib/memory/heap.h
@@ -2,6 +2,7 @@
 #define MEMORY_HEAP_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 // # pointer arithmetic! let's remember to compile this separately... because elevated privileges are a thing... 
 
@@ -10,30 +11,32 @@
 // heaps aka priority queues, so will likely need to decouple again... or might be worth implementing one in Cpp with templates?
 // Singly linked priority list data structure of memory nodes.
 typedef struct MemoryHeapNode {
-    void * ptr;
-    size_t block_size;
+    uint32_t index;
+    uint32_t block_size;
     MemoryHeapNode * child;
 } MemoryHeapNode;
 
 static bool sizeHeapPopCondition(MemoryHeapNode * node, void * arg) {
     // if (size <= node->block_size)
-    if (*((size_t *)arg) <= node->block_size) {
+    uint32_t size = *((uint32_t *)arg);
+    if (size <= node->block_size) {
         return true;
     }
     return false;
 }
 
-static bool ptrHeapPopCondition(MemoryHeapNode * node, void * ptr) {
+static bool ptrHeapPopCondition(MemoryHeapNode * node, void * arg) {
     // seems odd to cast like this but trust me?
-    if (ptr == node->ptr) {
+    uint32_t index = *((uint32_t *)arg);
+    if (index == node->index) {
         return true;
     }
     return false;
 }
 
-static bool mergeHeapPopCondition(MemoryHeapNode * node, void * ptr) {
-    // search for node->ptr < ptr and node->ptr + node->block_size == ptr.
-    if (ptr < node->ptr && node->ptr + node->block_size == ptr) {
+static bool mergeHeapPopCondition(MemoryHeapNode * node, void * arg) {
+    uint32_t index = *((uint32_t *)arg);
+    if (node->index + node->block_size == index) {
         return true;
     }
     return false;

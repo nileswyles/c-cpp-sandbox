f4fe43ce4a0881aee4c5c7c7c14662b411df299e
Kanye West <KanyeEast@notreally.com>
2024-05-10 07:38:22 -0400

    updated shell script to run tests too. Need to think about how to structure the test suite... (non-noob style)

--- build_scripts/enew_test.sh
+++ build_scripts/enew_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ROOT_DIR="."
+
 SRC_FILES="
 $ROOT_DIR/lib/test/enew_test.cpp
 $ROOT_DIR/lib/enew.cpp
@@ -7,9 +8,14 @@ $ROOT_DIR/lib/memory/emalloc.c
 $ROOT_DIR/lib/memory/heap.c
 "
 
+# see logger.h for log level values
 DEFINES="
 -DDYNAMIC_MEMORY_SIZE=16
--DLOGGER_LEVEL=1
+-DLOGGER_LEVEL=$1
 "
 
-g++ $SRC_FILES -iquote $ROOT_DIR/lib -iquote $ROOT_DIR/lib/memory $DEFINES -std=c++23 -o $ROOT_DIR/out/enew_test.out
+TEST_PATH=$ROOT_DIR/out/enew_test.out
+
+g++ $SRC_FILES -iquote $ROOT_DIR/lib -iquote $ROOT_DIR/lib/memory $DEFINES -std=c++23 -o $TEST_PATH
+
+exec $TEST_PATH

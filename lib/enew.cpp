c26c11a09f165169131dc8e8c4bb40d34a0c9c24
Kanye West <KanyeEast@notreally.com>
2024-05-11 04:47:26 -0400

    ...

--- lib/enew.cpp
+++ lib/enew.cpp
@@ -8,22 +8,18 @@
 
 void * operator new(std::size_t sz) {
     loggerPrintf(LOGGER_DEBUG, "(1) new(size_t), size = %zu\n", sz);
-    if (sz == 0)
-        ++sz; // avoid std::malloc(0) which may return nullptr on success
+    if (sz == 0) ++sz; // avoid std::malloc(0) which may return nullptr on success
 
-    if (void * ptr = malloc(sz))
-        return ptr;
+    if (void * ptr = malloc(sz)) return ptr;
 
     throw std::bad_alloc{};
 }
 
 void * operator new[](std::size_t sz) {
     loggerPrintf(LOGGER_DEBUG, "(2) new[](size_t), size = %zu\n", sz);
-    if (sz == 0)
-        ++sz; // avoid std::malloc(0) which may return nullptr on success
+    if (sz == 0) ++sz; // avoid std::malloc(0) which may return nullptr on success
 
-    if (void * ptr = malloc(sz))
-        return ptr;
+    if (void * ptr = malloc(sz)) return ptr;
 
     throw std::bad_alloc{};
 }

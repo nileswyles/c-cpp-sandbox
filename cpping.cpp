08e94065c6a6d3334174b4c7d70a4bb3c9857dde
Kanye West <KanyeEast@notreally.com>
2024-05-16 10:22:32 -0400

    cpp by example...

---
+++ cpping.cpp
@@ -0,0 +1,47 @@
+#include "stdio.h"
+
+class Test {
+    public:
+        size_t i;
+        // that is definetly valid cpp....?
+        // Test::Test(size_t i) : i(i) {
+        Test(size_t i) : i(i) {
+
+        } 
+
+        // Why do I need to define this?
+        Test() : i(1) {}
+        // Test::Test() : i(0) {}
+
+        // lol... 
+        static Test testConstructor(size_t i) {
+            return Test(i);
+        } 
+};
+
+int main() {
+    Test t(7);
+    Test t_constructor = Test(7);
+    Test t_constructor_default = Test();
+    Test t_constructor_func_static = Test::testConstructor(7);
+    printf("%lu\n", t.i);
+    printf("%lu\n", t_constructor.i);
+    printf("%lu\n", t_constructor_default.i);
+    printf("%lu\n", t_constructor_func_static.i);
+
+    // moral of the story... cpp defines "concepts" that don't fall in typical categories of function/variable/class? 
+    //  lol... I guess constructor is the category for Test()? 
+
+    // so...
+
+    // you can call constructor directly? 
+    //      Test()
+    // but also via other type of expresions... 
+    //      <type-name> <type-name>[()]; 
+    //      new <type-name>;
+
+    // hmm....
+    // interesting observation
+
+    return 1;
+}

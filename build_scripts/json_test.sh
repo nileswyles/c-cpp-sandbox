934670c3bd05c3cc44f66de4bbfbd68da31e2c9b
Kanye West <KanyeEast@notreally.com>
2024-05-17 16:53:03 -0400

    args to json_test.sh
    -l 3 -t testJson -D LOGGER_JSON_PARSER=1

--- build_scripts/json_test.sh
+++ build_scripts/json_test.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+LOG_LEVEL=0
+while true; do
+	case "$1" in
+		-l|--log) LOG_LEVEL="$2"; shift 2 ;;
+		-t|--test) TEST_ARG="$2"; shift 2 ;;
+		-D) DEFINES="$DEFINES -D$2"; shift 2 ;;
+		# --) shift; break ;;
+		*) break;;
+	esac
+done
+
+# see logger.h for log level values
+DEFINES=$DEFINES"
+-DLOGGER_LEVEL=$LOG_LEVEL
+"
+
 ROOT_DIR="."
 
 # Standardize this
@@ -13,11 +30,8 @@ $ROOT_DIR/lib/json/parser/json_array.cpp
 $ROOT_DIR/lib/c/array.c
 $ROOT_DIR/lib/test/tester.c
 "
-# see logger.h for log level values
-DEFINES="
--DLOGGER_LEVEL=$1
-"
+
 TEST_PATH=$ROOT_DIR/out/json_test.out
 rm $TEST_PATH
 g++ $SRC_FILES $TEST_C_LIB_PATH -iquote $QUOTE_INCLUDE_ROOT -iquote $ROOT_DIR/lib/json $DEFINES -Wno-pointer-arith -std=c++23 -o $TEST_PATH
-exec $TEST_PATH $2
+exec $TEST_PATH $TEST_ARG

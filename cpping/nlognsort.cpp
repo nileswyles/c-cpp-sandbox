cbce1a87a77f25c5af6601458a11c479c24c8a58
Niles Wyles <nileswiles@lol.com>
2024-07-04 01:47:56 +0000

    okay, hmm... even simpler... shifting solves all of those gap problems lmao...
    was originally trying to avoid that but didn't see that it basically solves two problems... nodes_visited should still be less than nnsort...

--- cpping/nlognsort.cpp
+++ cpping/nlognsort.cpp
@@ -3,14 +3,12 @@
 #define GRAPH_ENABLE 1
 
 template<typename T>
-size_t leftMerge(T * A, size_t sizeA, T * B, size_t sizeB, T *& swap_space, size_t& swap_space_size) {
+void leftMerge(T * A, size_t sizeA, T * B, size_t sizeB, T *& swap_space, size_t& swap_space_size) {
     size_t i = 0;
     size_t j = 0;
     T swap;
     T left_compare;
-    
-    size_t extension = 0;
-    while (i < sizeA + extension) {
+    while (i < sizeA) {
         left_compare = A[i];
         if (swap_space_size > 0) {
             left_compare = swap_space[0];
@@ -18,52 +16,26 @@ size_t leftMerge(T * A, size_t sizeA, T * B, size_t sizeB, T *& swap_space, size
         printf("i: %ld, swap_space_size(j): %ld, A[i]: %ld, B[j]: %ld, SS[i]: %ld\n", i, swap_space_size, A[i], B[swap_space_size], swap_space[0]);
         if (j < sizeB && left_compare > B[j]) {
             // B wins
-            if (i < sizeA) {
-                swap = A[i];
-                A[i] = B[j];
-                swap_space[swap_space_size] = swap;
+            swap = A[i];
+            A[i] = B[j];
+            swap_space[swap_space_size] = swap;
 
-                swap_space_size++;
-                j++;
-            } else {
-                // writing past original A array and into original swap_space/start of B (contiguous)
-
-                // here we just set the "deallocated" value to value at B and increment.
-                // this continues until B wins consecutively - extension number of times
-                // this also implies that when iterating past original A array, swap_space is never empty and the left_compare logic above is still valid... 
-                A[i] = B[j];
-                j++;
-            }
+            swap_space_size++;
+            j++;
+            // note, swap space extends further into B space...
         } else if (swap_space_size > 0) {
-            if (i < sizeA) {
-                // swap_space wins
-                swap = A[i];
-                A[i] = swap_space[0];
-                // hmm...
-                // so, we don't want the gap...
-                //  how do I elimate that? sizeA++ isn't actually fixing that...
- 
-                // so, if swap_space_wins, we place value at A[i] location at end of swap_space (preserving orderliness LMAO), that's a strict requirement...
-                // so, I guess only really solution is to track gaps...?
- 
-                // actually, original idea was to continue iterating so that 
-                //  as alternative to explicitly tracking the gaps and passing that info across invocations of this function...
-                //  what if, I check if past original sizeA, and if so, don't swap... just consume swap_space...
-                //  algorithm works as normally expected and 
-                //  then no gap, :)
-                swap_space++;
-                swap_space[swap_space_size] = swap;
-                // size of swap space remains the same.
-            } else {
-                // writing past original A array and into original swap_space/start of B (contiguous)
-
-                // swap space won at some point and need to preserve orderliness, so in order to avoid gap, let's extend iterations and just consume swap space if beyond original sizeA (again, assuming contiguous)
-                // here we just consume swap_space and similiar to the normal case tell the algorithm go even further and write to an extra value past original A (extension++).
-                A[i] = swap_space[0];
-                swap_space++;
-                swap_space_size--;
+            swap = A[i];
+            A[i] = swap_space[0];
+            // unnavoidable shifting...
+            size_t index_of_last_element = swap_space_size - 1;
+            for (size_t i = 0; i < index_of_last_element; i++) {
+                swap_space[i] = swap_space[i + 1];
+                nodes_visited++;
             }
-            extension++;
+            // set new value at end of swap space
+            swap_space[index_of_last_element] = swap;
+
+            // note, size of swap space remains the same...
         } // else swap_space empty and A wins
 
         // okay, now if swap_space is empty....
@@ -72,7 +44,6 @@ size_t leftMerge(T * A, size_t sizeA, T * B, size_t sizeB, T *& swap_space, size
         i++;
         nodes_visited++;
     }
-    return j;
 }
 
 template<typename T>
@@ -80,7 +51,7 @@ template<typename T>
 void merge(T * A, size_t sizeA, T * B, size_t sizeB) {
     size_t swap_space_size = 0;
     T * swap_space = B;
-    size_t B_index = leftMerge(A, sizeA, B, sizeB, swap_space, swap_space_size);
+    leftMerge(A, sizeA, B, sizeB, swap_space, swap_space_size);
     bool first_run = true;
     // then again. if swap_space isn't empty merge swap_space with remaining B
     while (first_run || swap_space_size > 1) {
@@ -88,31 +59,12 @@ void merge(T * A, size_t sizeA, T * B, size_t sizeB) {
 
         A = swap_space;
         sizeA = swap_space_size;
-
-        // hmmm... I think that's it...
-
-        // so, typically, swap_space == B but if swap_space won at some point AND then extended and B won, 
-        //  then there's a gap between end of swap_space and start of unsorted elements...
-
-        // now, think about how repeating over and over is affected by this...
-        // we'll iterate over the new swap_space... 
-
-        // hmm... I think this might be it...
-
-        B = &B[B_index];
+        B = &B[swap_space_size];
         swap_space = B;
-        sizeB -= B_index;
+        sizeB -= swap_space_size;
         swap_space_size = 0;
         leftMerge(A, sizeA, B, sizeB, swap_space, swap_space_size);
-
-        // okay, so repeat until single element in swap_space... hmm... this is increasingly more dumb lol..
-        //  inherent time/space trade-off?
     }
-    // if odd size before split,
-    // j may not equal sizeB but that's fine because it should already be sorted... 
-
-    // Yeah, so now the question is whether these extra operations are worse than allocating a new array...
-    //  probably splitting hairs (hares?) lol - but interesting...
 }
 
 // #define ARRAY_SIZE 7

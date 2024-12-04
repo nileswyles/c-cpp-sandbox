8d9cccac1507f069de4e2be094929f392b9ae8b1
Niles Wyles <nileswiles@lol.com>
2024-12-04 21:22:56 +0000

    implemented dynamic and fixed sized thread pool instead of the basic limit. threads are reused instead of tearing down and initializing again.

--- lib/threads/etasker.cpp
+++ lib/threads/etasker.cpp
@@ -4,46 +4,57 @@
 
 using namespace WylesLibs;
 
-void ETasker::setThreadTimeout(uint32_t ts) {
+void ETasker::setThreadTimeout(uint64_t ts) {
     pthread_mutex_lock(&this->mutex);
     pthread_t pthread = pthread_self();
-    thread_pool_queue[pthread].timeout_s = ts;
+    this->thread_pool_queue[pthread].timeout_s = ts;
     pthread_mutex_unlock(&this->mutex);
 }
 
 uint64_t ETasker::getThreadTimeout() {
-    // lol...
     pthread_mutex_lock(&this->mutex);
     pthread_t pthread = pthread_self();
-    uint64_t timeout_s = thread_pool_queue[pthread].timeout_s;
+    uint64_t timeout_s = this->thread_pool_queue[pthread].timeout_s;
     pthread_mutex_unlock(&this->mutex);
     return timeout_s;
 }
 
 void ETasker::run(ESharedPtr<ETask> task) {
     pthread_mutex_lock(&this->mutex);
-    if (thread_limit != SIZE_MAX && thread_pool.size() > thread_limit) {
-        thread_pool_queue.push_back(task);
+    if (this->thread_limit != SIZE_MAX && this->thread_pool.size() > this->thread_limit) {
+        this->thread_pool_queue.push_back(task);
     } else {
         this->taskRun(task);
     }
     pthread_mutex_unlock(&this->mutex);
 }
 
+void ETasker::taskRun(ESharedPtr<ETask> task) {
+    pthread_t thread;
+    ThreadArg * arg = new ThreadArg(task);
+    int ret = pthread_create(&thread, &this->attr, this->threadContext, arg);
+    if (ret != 0) {
+        std::string msg("Failed to create ETasker thread.");
+        loggerPrintf(LOGGER_INFO, "%s\n", msg.c_str());
+        throw std::runtime_error(msg);
+    }
+}
+
 void ETasker::timerProcess(void * arg) {
     while(1) {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&this->mutex);
         for (auto i: this->thread_pool) {
+            pthread_t pthread = i.first;
             EThread ethread = i.second;
             struct timespec t;
             clock_gettime(CLOCK_MONOTONIC, &t);
             loggerPrintf(LOGGER_DEBUG_VERBOSE, "fd: %d, start: %ld, timeout: %u, current_time: %lu\n", ethread.start_time.tv_sec, ethread.timeout_s, t.tv_sec);
             if (ethread.start_time.tv_sec + ethread.timeout_s <= t.tv_sec) {
                 loggerPrintf(LOGGER_DEBUG, "Thread expired sending signal.\n");
-                raise(SIGKILL);
+                pthread_kill(pthread, SIGKILL);
             }
         }
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&this->mutex);
         sleep(1); // 1s
     }
 }
@@ -54,54 +65,65 @@ void * ETasker::threadSigAction(int sig, siginfo_t * info, void * context) {
 
 void * ETasker::threadContext(void * arg) {
     ThreadArg * a = (ThreadArg *)arg;
-    ESharedPtr<ETask> task = a->task;
-    std::string thread_id; // or pointer, TODO:
-    delete a; // free before function call, so that it can terminate thread however it wants... 
-
     // ! IMPORTANT - every application must implement a sig handler that processes SIGKILL.
     WylesLibs::registerThreadSigActionHandler(this->threadSigAction);
 
-    pthread_mutex_lock(&mutex);
-    thread_pool[pthread_self()] = EThread(task);
-    pthread_mutex_unlock(&mutex);
+    jmp_buf * env = new jmp_buf;
+    pthread_t pthread = pthread_self();
 
+    pthread_mutex_lock(&this->mutex);
+    this->thread_pool[pthread] = EThread(a->task, ESharedPtr<jmp_buf>(env));
+    pthread_mutex_unlock(&this->mutex);
+
+    delete a; // free before function call, so that it can terminate thread however it wants... 
+    ::setjmp(env);
+
+    ESharedPtr<ETask> task = this->thread_pool[pthread].task;
+    if (true == task.isNullPtr()) {
+        while (this->thread_pool_queue.size() == 0) {} // fixed thread pool loop block while waiting for task.
+
+        pthread_mutex_lock(&this->mutex);
+        if (this->thread_pool_queue.size() > 0) {
+            this->thread_pool[pthread].task = this->thread_pool_queue.front();
+            this->thread_pool_queue.pop_front();
+        } else {
+            ::longjmp(ESHAREDPTR_GET_REF(this->thread_pool[pthread].env), 1);
+        }
+        pthread_mutex_unlock(&this->mutex);
+    }
     task->initialize();
     task->run();
 
-    // graceful thread teardown
-    this->threadTeardown();
+    if (true == this->fixed) {
+        this->thread_pool[pthread].task = nullptr;
+        ::longjmp(ESHAREDPTR_GET_REF(this->thread_pool[pthread].env), 1);
+    } else {
+        // graceful thread teardown
+        //  if items are queued, it will loop back to beginning of thread context using longjmp, otherwise it tears down the thread.
+        this->threadTeardown();
+    }
 
     return NULL;
-}
-
-void ETasker::taskRun(ESharedPtr<ETask> task) {
-    // TODO: is it okay to share the attr? lol lol
-    pthread_t thread;
-    ThreadArg * arg = new ThreadArg(thread_id, task);
-    int ret = pthread_create(&thread, &this->attr, threadContext, arg);
-    if (ret != 0) {
-        std::string msg("Failed to create ETasker thread.");
-        loggerPrintf(LOGGER_INFO, "%s\n", msg.c_str());
-        throw std::runtime_error(msg);
-    }
 }
 
 void ETasker::threadTeardown() {
     pthread_mutex_lock(&this->mutex);
 
-    pthread_t current_pthread = pthread_self();
-    EThread ethread = thread_pool[current_pthread];
+    pthread_t pthread = pthread_self();
+    EThread ethread = this->thread_pool[pthread];
     ethread.task->onExit();
-    thread_pool.erase(current_pthread);
 
     // TODO:
     //      if this fails, we don't retry. just the way it is for now.
-    if (thread_pool_queue.size() > 0) {
-        taskRun(thread_pool_queue.pop_front());
-    }  
+    if (this->thread_pool_queue.size() > 0) {
+        this->thread_pool[pthread].task = nullptr;
 
-    pthread_mutex_unlock(&this->mutex);
-
-    // semi-colon; (line-separator)
-    pthread_exit();
+        pthread_mutex_unlock(&this->mutex);
+        ::longjmp(ESHAREDPTR_GET_REF(ethread.env), 1);
+    } else {
+        this->thread_pool.erase(pthread);
+        pthread_mutex_unlock(&this->mutex);
+        // semi-colon; (line-separator)
+        pthread_exit();
+    }
 }
--- lib/threads/etasker.h
+++ lib/threads/etasker.h
@@ -19,11 +19,91 @@
 
 //              similarly, the queued tasks will move on to next.
 //          queued tasks spawn a new thread when popped from queue.
-#include <list>
 
+// there are various async-multithreading models
+//      the models supported here:
+//          1-1 thread per task --- thread is tore down after each task... "unlimited"/os-limited number of concurrent threads
+//          many-1 thread per task --- thread is reused if a task is available after current task is finished... limited number of concurrent threads
+//              dynamic threads
+//                  you can decide whether to teardown if queue is empty or 
+//                      this is in some ways an optimization of the 1-1 model.
+//                  you can definetly support reuse if a normal teardown... 
+//                  it's questionable if you can support reuse if preempted. 
+//                  need to see if longjmp, exceptions work in signal action context.
+//                  otherwise, you can simply relaunch a thread.
+//                  
+//                  Continuing on eliminating churn, 
+//                      Do we want to wait a while for new task before tearing down thread? in other words, dampen the thread teardown?
+//                      when would this be useful?
+//                          maybe bursty applications? let's not over-engineer for now.
+
+//              fixed-set threads
+//                  keep a fixed-set of threads and poll until new task if queue is empty.
+//                  it seems dumb to loop while doing nothing - this doesn't seem valuable.
+//                  
+//                  an argument to implement thread pools as a fixed-set is to avoid that coupling... so maybe in situations where that coupling is an issue, avoid.
+//                  so, maybe allow this option aswell.
+//          
+//          furthermore, think about some more use-cases
+//              think javascript - promises/callbacks
+//                  This can be implemented using any of the models outlined above.
+//                  task functions are essentially a "callback" but you can build more user-friendly abstractions leveraging that? 
+//                  some sort of event loop? where tasks/events are sourced from a single thread, etc. 
+//                  no need to restrict to this use-case but useful for conceptualizing (idk)?
+//
+//          some other models...
+//              specific to the socket/fd context.... lol.
+//                  considering this model, you might multiplex at each task instead of 1-1 fd to task...
+//                  maybe poll if set of fds are available and perform action... this seems overly engineered, complicated and seems to have less performance.
+//              
+
+//          key takeaways/principles:
+//              1. when thinking about creating useful abstractions, consider application use-cases...
+//                  a. process for sort of coming up with an optimal model of abstraction:
+//                      1. start at simpilest solution... this will very often offer the most performance.
+//                          a. in many cases you can make improvements without affecting performance.
+//                      2. some intuition is needed to build the set of possible abstractions/optimizations/improvements - are you optimizing for simplicity, performance, both, etc?
+//                      3. once you have the set of possible improvements, choose one.
+//                      4. keep in mind what the motivations were for the improvement (simplicity, performance). 
+//                          a. how do we characterize these metrics?
+//                              a. for performance (and sort of the idea I really wanted to jot down)
+//                                     limit time to execute some function?
+//                                          limiting the sub functions.
+//                                          limit the time to execute sub functions.
+//                                     more specifically, in the example of eliminating thread churn (similar to caching policies?)
+//                                          thread churn is a function of the invocation curve. in other words, the performance of sub functions depends on when and how often the function is invoked with respect to time..
+//                                          alright, so let's define a type of function that is characterized by the above.
+//                                              in this example, limiting number of concurrent threads, created a situation where you will need to queue items. Which enabled the optimization of reusing threads instead of tearing down.
+//                                                  the need to queue items enabled dynamic threading, adding another dimension of complexity 
+//                                                      but in a way that is coupled to how the function is invoked... 
+//                                                          it's dependent on a "hidden"/ignored variable.
+
+//                                          what invocation curves with respect to time will benefit from this optimization?
+//                                              so, when is thread churned (lol)? timeout and no items queued
+//                                                  the invocation curve with respect to time affects th
+//                                          
+//                      5. will the improvement affect other metrics (not primary motivations for improvement but still needed)
+//                          a. decide whether to commit.
+
+//                  I sound like a broken record lol, consolidate this into a notion page. Original thought is important.
+
+//          actionable items:
+//              1. many-1 thread per task - 
+//                  answer questions surrounding whether exceptions or longjmp work in signal action context. review how exceptions and longjmp work.
+//              2. dynamic and fixed thread pooling
+
+#include "memory/pointers.h"
+#include "string_format.h"
+
+#include <list>
+#include <map>
+
+#include <stddef.h>
+#include <setjmp.h>
 #include <pthread.h>
 #include <time.h>
 #include <stdbool.h>
+#include <sys/sysinfo.h>
 
 #ifndef LOGGER_ETASKER
 #define LOGGER_ETASKER 1
@@ -50,19 +130,19 @@ namespace WylesLibs {
             struct timespec start_time;
             uint64_t timeout_s;
             ESharedPtr<ETask> task;
-            EThread(ESharedPtr<ETask> ta, uint32_t ts) {
+            ESharedPtr<jmp_buf> env; // ! IMPORTANT - since queuing is a common and exceptions are expensive, longjmp and setjmp to return back to thread context from signal preemption.
+            EThread(ESharedPtr<ETask> ta, uint64_t ts, ESharedPtr<jmp_buf> env) {
                 task = ta;
 
                 clock_gettime(CLOCK_MONOTONIC, &start_time);
-                start_s = t;
                 timeout_s = ts;
+                env = env;
             }
             ~EThread() = default;
     };
 
     typedef struct ThreadArg {
-        ESharedPtr<task> task;
-        std::string thread_id; // or pointer? TODO:
+        ESharedPtr<ETask> task;
     } ThreadArg;
     
     class ETasker {
@@ -74,32 +154,43 @@ namespace WylesLibs {
             pthread_mutex_t mutex;
             pthread_t timer_thread;
             pthread_attr_t attr;
+            bool fixed;
 
+            void taskRun(ESharedPtr<ETask> task);
             void timerProcess(void * arg);
             void * threadSigAction(int sig, siginfo_t * info, void * context);
             void * threadContext(void * arg);
-            void taskRun(ESharedPtr<ETask> task);
             void threadTeardown();
         public:
             uint64_t initial_timeout_s;
-            ETasker(): ETasker(SIZE_MAX, DEFAULT_ETASKER_TIMEOUT_S) {}
-            ETasker(size_t tl): ETasker(tl, DEFAULT_ETASKER_TIMEOUT_S) {
-            ETasker(size_t tl, uint64_t ts) {
-                pthread_attr_init(&attr);
+            ETasker(): ETasker(SIZE_MAX, DEFAULT_ETASKER_TIMEOUT_S, false) {}
+            ETasker(size_t tl): ETasker(tl, DEFAULT_ETASKER_TIMEOUT_S, false) {
+            ETasker(size_t tl, uint64_t ts, bool fixed) {
+                pthread_attr_init(&attr); // ! IMPORTANT - pthread_create arg is const.
                 pthread_attr_setdetachstate(&attr, 1);
                 thread_limit = tl;
                 initial_timeout_s = ts;
                 
                 pthread_mutex_init(&mutex, nullptr);
                 pthread_create(&timer_thread, &attr, timerProcess, NULL);
+                fixed = fixed;
+
+                if (true == fixed) {
+                    int nprocs = get_nprocs();
+                    if (tl > nprocs) {
+                        std::string msg = WylesLibs::format("Fixed thread pool cannot be of larger than nprocs {d}", nprocs);
+                        loggerPrintf(LOGGER_DEBUG_VERBOSE, "%s\n", msg.c_str());
+                        throw std::runtime_error(msg);
+                    }
+                }
             } // any other thread paramaters.
 
             ~ETasker() {
-                pthread_kill(&this->timer_thread, SIGKILL);
+                pthread_kill(this->timer_thread, SIGKILL);
                 pthread_mutex_destroy(&this->mutex);
             }
 
-            void setThreadTimeout(uint32_t ts);
+            void setThreadTimeout(uint64_t ts);
             uint64_t getThreadTimeout();
             void run(ESharedPtr<ETask> task);
     };
